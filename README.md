34a06b9fa9dce5feac165a8982047f8c1212586f
Dan Flettre <flettre@gmail.com>
2016-11-30 03:17:01 -0600

    initial

---
+++ README.md
@@ -0,0 +1,34 @@
+# playground
+
+playground website for api-playground
+
+[![npm][npm-image]][npm-url]
+[![travis][travis-image]][travis-url]
+[![standard][standard-image]][standard-url]
+
+[npm-image]: https://img.shields.io/npm/v/playground.svg?style=flat-square
+[npm-url]: https://www.npmjs.com/package/playground
+[travis-image]: https://img.shields.io/travis/Flet/playground.svg?style=flat-square
+[travis-url]: https://travis-ci.org/Flet/playground
+[standard-image]: https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg?style=flat-square
+[standard-url]: http://npm.im/semistandard
+
+## Install
+
+```
+npm install playground
+```
+
+## Usage
+
+```js
+var playground = require('playground')
+```
+
+## Contributing
+
+Contributions welcome! Please read the [contributing guidelines](CONTRIBUTING.md) first.
+
+## License
+
+[MIT](LICENSE.md)
---
+++ index.js
@@ -0,0 +1,154 @@
+'use strict';
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var app = express();
+var get = require('simple-get');
+var qs = require('qs');
+
+const apiUrl = process.env.PLAYGROUND_URL;
+console.log('api url', apiUrl);
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('static'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', (req, res) => {
+  var query = {
+    id: {
+      $in: topLevelCategories
+    }
+  };
+
+  var opts = {
+    method: 'GET',
+    url: `${apiUrl}/categories?${qs.stringify(query)}`,
+    json: true
+  };
+
+  get.concat(opts, (err, resp, data) => {
+    if (err) throw err;
+    res.render('index', {categories: data.data});
+  });
+});
+
+app.get('/product/:id', (req, res) => {
+  var opts = {
+    method: 'GET',
+    url: `${apiUrl}/products/${req.params.id}`,
+    json: true
+  };
+
+  get.concat(opts, (err, resp, data) => {
+    if (err) throw err;
+    res.render('product', data);
+  });
+});
+
+app.get('/search', (req, res) => {
+  var page = Number(req.query.page) || 1;
+  var limit = 10;
+
+  var query = {
+    $skip: (page * limit) - limit
+  };
+
+  if (req.query.q) {
+    query.$or = {
+      name: { '$like': `%${req.query.q}%` },
+      description: { '$like': `%${req.query.q}%` }
+    };
+  } else if (req.query.cat) {
+    query['category.id'] = req.query.cat;
+  }
+
+  var opts = {
+    method: 'GET',
+    url: `${apiUrl}/products?${qs.stringify(query)}`,
+    json: true
+  };
+
+  get.concat(opts, (err, resp, data) => {
+    if (err) throw err;
+
+    data.page = page;
+    data.nextPage = page + 1;
+
+    data.pages = makePages(page, data.total, data.limit);
+
+    data.next = data.limit + data.skip;
+
+    if (req.query.cat) {
+      let catOpts = {
+        method: 'GET',
+        url: `${apiUrl}/categories/${req.query.cat}`,
+        json: true
+      };
+      get.concat(catOpts, (err, resp, catData) => {
+        data.cat = catData.id;
+        data.catName = catData.name;
+        data.categoryPath = catData.categoryPath;
+        data.subCategories = catData.subCategories;
+        res.render('catresults', data);
+      });
+    } else {
+      data.q = req.query.q;
+      res.render('productresults', data);
+    }
+  });
+});
+
+var port = process.env.PORT || 4000;
+
+app.listen(port, function (err) {
+  if (err) {
+    console.error(err);
+  }
+
+  console.log(`App is up and listening on ${port}`);
+});
+
+function makePages (page, total, limit) {
+  var pages = [];
+  let spread = 6;
+  let partialPage = (total % limit);
+  let lastPage = (total - partialPage) / limit;
+  if (partialPage > 0) lastPage++;
+
+  for (let i = page - 1; i > 0 && i > page - 3; i--) {
+    pages.unshift({page: i, current: false});
+  }
+
+  pages.push({page: page, current: true});
+
+  for (let i = page + 1; i <= lastPage && pages.length < spread; i++) {
+    pages.push({page: i, current: false});
+  }
+
+  if (pages.length === 1) pages = [];
+  var pageData = {
+    pages: pages
+  };
+
+  if (page > 1) pageData.previous = page - 1;
+  if (page < lastPage) pageData.next = page + 1;
+  return pageData;
+}
+
+var topLevelCategories = [
+  'abcat0100000',
+  'abcat0200000',
+  'abcat0207000',
+  'abcat0300000',
+  'abcat0400000',
+  'abcat0500000',
+  'abcat0600000',
+  'abcat0700000',
+  'abcat0800000',
+  'abcat0900000'
+];
